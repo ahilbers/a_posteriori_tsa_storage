05f581f36b885763b1e468c0a556106ca734bccc
Adriaan P Hilbers <a.hilbers17@imperial.ac.uk>
2022-10-01 14:41:53 +0200

    prepare example simulations

--- scripts/main_example.sh
+++ scripts/main_example.sh
@@ -9,12 +9,12 @@
 # ----------
 SIMULATION_NAME_LIST=(
     "agg_inp_mean"  # Method A
-    # "agg_inp_closest"  # Method B: (a priori) first stage of method F
-    # "agg_str_gencost_op_vars"  # Method F: (a posteriori) second stage
+    "agg_inp_closest"  # Method B: (a priori) first stage of method F
+    "agg_str_gencost_op_vars"  # Method F: (a posteriori) second stage
 )
 SIMULATION_TYPE_LIST=("get_design_estimate" "get_operate_variables")
-TS_BASE_RESAMPLE_NUM_YEARS=1
-TS_REDUCTION_NUM_DAYS=10
+TS_BASE_RESAMPLE_NUM_YEARS=30
+TS_REDUCTION_NUM_DAYS=120
 
 
 # Simulations
@@ -23,7 +23,7 @@ for SIMULATION_NAME in "${SIMULATION_NAME_LIST[@]}"
 do
     for SIMULATION_TYPE in "${SIMULATION_TYPE_LIST[@]}"
     do
-        for REPLICATION in {0..0}
+        for REPLICATION in {0..1}
         do
             python3 main.py \
                     --simulation_name $SIMULATION_NAME \
