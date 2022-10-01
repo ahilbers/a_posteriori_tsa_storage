d31766453fbd555cf001619a85b7ed3c20457aa3
Adriaan P Hilbers <a.hilbers17@imperial.ac.uk>
2022-10-01 16:11:16 +0200

    create master script

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,18 +1,13 @@
-# Simulate Imperial HPC batch job in laptop
+# Run all simulations and create plots
 
-export PBS_JOBNAME="laptop"
-PBS_ARRAY_INDEX_FIRST=1
-PBS_ARRAY_INDEX_LAST=1
+printf "Running example simulations.\n\n"
+# sh scripts/run_example.sh
+printf "Done running example simulations.\n\n\n\n\n\n"
 
-for PBS_ARRAY_INDEX_VALUE in $(seq $PBS_ARRAY_INDEX_FIRST $PBS_ARRAY_INDEX_LAST)
-do
-    # If PBS_ARRAY_INDEX = 0, then run all simulations inside python in one go
-    export PBS_ARRAY_INDEX=$PBS_ARRAY_INDEX_VALUE
-    sh main_validation.sh
-    printf "\n\n\n\n"
-done
+printf "Running example simulations.\n\n"
+# sh scripts/run_validation.sh
+printf "Done running example simulations.\n\n\n\n\n\n"
 
-
-# Clean up outputs directory
-# python3 organise_outputs.py collate filename=summary_outputs.csv
-# python3 organise_outputs.py clean
+printf "Creating plots.\n\n"
+sh scripts/make_figures.sh
+printf "Done.\n"
